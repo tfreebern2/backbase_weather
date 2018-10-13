1bfa8a9cedd8e472e5586db216f92174badc1d02
Timothy Freebern <timothy.freebern@capco.com>
2018-10-13 17:21:21 -0400

    Created Location model class.

---
+++ app/src/main/java/com/capco/freebern/tim/weatherapp/location/model/Location.java
@@ -0,0 +1,37 @@
+package com.capco.freebern.tim.weatherapp.location.model;
+
+public class Location {
+
+    private double latitude;
+    private double longitude;
+    private String name;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
