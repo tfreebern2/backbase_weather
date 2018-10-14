289b5e349891b8b399f3c0040bfeeebaf9a76b22
Timothy Freebern <timothy.freebern@capco.com>
2018-10-14 19:42:43 -0400

    Added Image Button for bookmarked Locations in the MapsActivity

--- app/src/main/java/com/capco/freebern/tim/weatherapp/map/MapsActivity.java
+++ app/src/main/java/com/capco/freebern/tim/weatherapp/map/MapsActivity.java
@@ -1,9 +1,14 @@
 package com.capco.freebern.tim.weatherapp.map;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.capco.freebern.tim.weatherapp.R;
+import com.capco.freebern.tim.weatherapp.location.LocationActivity;
 import com.capco.freebern.tim.weatherapp.location.model.Location;
 import com.capco.freebern.tim.weatherapp.map.marker.MarkerManager;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private DatabaseReference mDatabaseReference;
+    private ImageButton mLocationsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         mDatabaseReference = FirebaseDatabase.getInstance().getReference("locations");
+        mLocationsButton = (ImageButton) findViewById(R.id.locationsButton);
+
+        mLocationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent locationsIntent = new Intent(MapsActivity.this, LocationActivity.class);
+                startActivity(locationsIntent);
+            }
+        });
     }
 
     @Override
@@ -50,16 +65,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
             @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {}
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {
+            }
 
             @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
 
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {
+            }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {}
+            public void onCancelled(DatabaseError databaseError) {
+            }
         });
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
